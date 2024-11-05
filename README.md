7f4500ec18bc76baadf9ae302d3ab9f5c436d621
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-11-05 14:04:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Praktikum 1: Mengambil Foto dengan Kamera
+
+## Langkah 1: Buat Project Baru
+
+## Langkah 2: Tambah dependensi yang diperlukan<br>
+
+![alt text](image.png)<br>
+
+## Langkah 3: Ambil Sensor Kamera dari device<br>
+
+![alt text](image-1.png)<br>
+![alt text](image-2.png)<br>
+
+## Langkah 4: Buat dan inisialisasi CameraController<br>
+
+## Langkah 5: Gunakan CameraPreview untuk menampilkan preview foto<br>
+
+## Langkah 6: Ambil foto dengan CameraController<br>
+
+Praktikum Langkah 4-6:<br>
+![alt text](code8.png)<br>
+
+## Langkah 7: Buat widget baru DisplayPictureScreen
+
+![alt text](image-3.png)<br>
+
+## Langkah 8: Edit main.dart
+
+![alt text](image-4.png)<br>
+
+## Langkah 9: Menampilkan hasil foto
+
+![alt text](image-5.png)<br>
+![alt text](<WhatsApp Image 2024-11-05 at 13.48.37_534809a3.jpg>)<br>
+![alt text](<WhatsApp Image 2024-11-05 at 13.48.37_9d89a9cf.jpg>)<br>
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:kamera_flutter/widget/takepicture_screen.dart';
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  const MyApp({super.key, required this.camera});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(camera: camera, title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final CameraDescription camera;
+  final String title;
+
+  const MyHomePage({super.key, required this.camera, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.high,
+    );
+    _controller.initialize().then((_) {
+      if (!mounted) return;
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_controller.value.isInitialized) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: CameraPreview(_controller),
+    );
+  }
+}
